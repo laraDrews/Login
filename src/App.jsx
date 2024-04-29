f6fe471be3fc44d648d801b225ff666e77c322f0
Lara Drews <lara_drews@estudante.sesisenai.org.br>
2024-04-29 09:27:35 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,24 @@
-// src/App.jsx
+import './App.css'
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cadastro from './pages/cadastro';
+import Home from './pages/home';
+import Login from './pages/login';
 import axios from 'axios';
 
-const logar = async (username, password) => {
- try {
- const response = await axios.post('http://localhost:8090/api/login', {
- username: username,
- password: password,
- });
- return response.data;
- } catch (error) {
- throw error;
- }
-};
 function App() {
- const [username, setUsername] = useState('');
- const [password, setPassword] = useState('');
- const handleLogin = async () => {
- try {
- const response = await logar(username, password);
- alert(response);
- } catch (error) {
- console.error('Erro ao se logar:', error);
- }
- };
- return (
- <div>
- <h1>Login System</h1>
- <form>
- <label>
- Usuário:
- <input
- type="text"
- value={username}
- onChange={(e) => setUsername(e.target.value)}
- />
- </label>
- <br />
- <label>
- Senha:
- <input
- type="password"
- value={password}
- onChange={(e) => setPassword(e.target.value)}
- />
- </label>
- <br />
- <button type="button" onClick={handleLogin}>
- Login
- </button>
- </form>
- </div>
- );
-}
-export default App;
+    return (
+      <Router>
+        {}
+        <Routes>
+          {}
+          <Route path="/Home" element={<Home />} />        
+         {}
+          <Route path="/Cadastro" element={<Cadastro />} />
+          {}
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    );
+  }
+  export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css'
+
+
+const logar = async (username, password) => {
+    try {
+        const response = await axios.post('http://localhost:8090/api/login', {
+            username: username,
+            password: password,
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+function Login(){
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/cadastro');
+    };
+
+       
+
+        const [username, setUsername] = useState('');
+        const [password, setPassword] = useState('');
+        const handleLogin = async () => {
+            try {
+                const response = await logar(username, password);
+                
+                if(response == true){
+                     navigate("/home")
+                     alert("Logado com sucesso!")
+                }
+            } catch (error) {
+                console.error('Erro ao se logar:', error);
+            }
+        };
+        return (
+            <div>
+                <h1>Login System</h1><br/>
+                <div className="info">
+                <form>
+                    <label>
+                        Usuário  
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <br/><br/>
+                    <label>
+                        Senha    
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+                    <br />    <br/>
+                    <button type="button" onClick={handleLogin}>
+                        Entrar  
+                    </button>    <br/> <br/>
+                </form>                 
+                <a href="/cadastro"> Não possui conta? Cadastre-se</a>
+            </div>
+            </div>
+    );
+}
+export default Login;
